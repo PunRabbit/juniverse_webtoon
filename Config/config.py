275c66f080f93c70573738723ce40a54d467388f
PunRabbit <tjdmsch2201@gmail.com>
2022-01-20 23:45:26 +0900

    2022. 1. 20 - 슬픈 야근 후 기본 Structure 업데이트 (작업중)

---
+++ Config/config.py
@@ -0,0 +1,19 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Config:
+
+    DATABASE_PORT_NUMBER: int = 3306
+    DATABASE_ADDRESS: str = '127.0.0.1'
+    DATABASE_USER_NAME: str = 'root'
+    DATABASE_USER_PASSWORD: str = 'webtoon'
+
+    SERVER_URL_ADDRESS: str = '0.0.0.0'
+    SERVER_URL_SCHEME: str = 'http'
+    SERVER_PORT_NUMBER: int = 8000
+
+
+
+
+
