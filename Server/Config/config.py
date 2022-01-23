0adaf852874d468fd1da41d0fe60a523705fe9bf
PunRabbit <tjdmsch2201@gmail.com>
2022-01-23 22:09:41 +0900

    2022. 1. 22 - dotenv 추가, config.py 반영

--- Server/Config/config.py
+++ Server/Config/config.py
@@ -1,18 +1,22 @@
+import os
 from dataclasses import dataclass
+from dotenv import load_dotenv
+
+load_dotenv(verbose=True)
 
 
 @dataclass
 class Config:
 
-    DATABASE_NAME: str = "juniverse"
-    DATABASE_PORT_NUMBER: int = 3306
-    DATABASE_ADDRESS: str = '127.0.0.1'
-    DATABASE_USER_NAME: str = 'root'
-    DATABASE_USER_PASSWORD: str = 'webtoon'
+    DATABASE_NAME: str = os.getenv('database_name')
+    DATABASE_PORT_NUMBER: int = int(os.getenv('database_port_number'))
+    DATABASE_ADDRESS: str = os.getenv('database_address')
+    DATABASE_ROOT_USER_NAME: str = os.getenv('database_root_user_name')
+    DATABASE_ROOT_USER_PASSWORD: str = os.getenv('database_root_user_password')
 
-    SERVER_URL_ADDRESS: str = '0.0.0.0'
-    SERVER_URL_SCHEME: str = 'http'
-    SERVER_PORT_NUMBER: int = 8000
+    SERVER_URL_ADDRESS: str = os.getenv('server_url_address')
+    SERVER_URL_SCHEME: str = os.getenv('server_url_scheme')
+    SERVER_PORT_NUMBER: int = int(os.getenv('server_port_number'))
 
 
 config_args = Config()
